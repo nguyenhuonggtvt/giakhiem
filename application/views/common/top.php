bd40176fb0fdd5fb7729767720a6fa1bf306997f
Nguyen Huong <nguyenvanhuonghg92@gmail.com>
2019-05-11 19:52:13 +0700

    SEO url twitter

--- application/views/common/top.php
+++ application/views/common/top.php
@@ -10,8 +10,13 @@
 	<meta property="og:type" content="{$site_type}"/>
 	<meta property="og:title" content="{$title}"/>
 	<meta property="og:description" content="{$description}"/>
+	<meta property="og:url" content="{current_url()}" />
 	<meta property="og:site_name" content="{$site_name}"/>
 	<meta property="og:image" content="{if isset($image_header)}{$image_header}{/if}"/>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:description" content="{$description}" />
+	<meta name="twitter:title" content="{$title}" />
+	<meta name="twitter:image" content="{if isset($image_header)}{$image_header}{/if}" />
 	
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 	
